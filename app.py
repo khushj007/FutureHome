88e36b58feb823e2778a4d3a0404d302e637b07f
khush <khushjoshi007@gmail.c/usr/local/bin/python3 /Users/HCjoshi/Desktop/DSMP2.0/MLops/class5/pipeline/src/validation.pyom>
2024-07-02 09:53:44 +0530

    price prediction module added

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgram Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to FutureHome! 👋")
+st.write("FutureHome combines innovative data analytics with real estate expertise to offer accurate property predictions, thorough analysis, and personalized recommendations")
